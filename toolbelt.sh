ab326700c0c4024f56532938c188df394daf2588
skywarth <yigitk.ersoy@gmail.com>
2021-07-25 19:35:44 +0300

    json parser to associative and recursive array is finally done

--- toolbelt.sh
+++ toolbelt.sh
@@ -13,3 +13,68 @@ done < $1
 # usage example below
 # Toolbelt_readConfig config-file-location
 # Toolbelt_readConfig ./some-config.txt
+
+
+
+
+
+
+Toolbelt_parseJSON(){
+     local -n jsonArray=$2
+  jsonArray[0]="rrr"
+
+  SETTINGS_FILE="$1"
+  SETTINGS_JSON=$(cat $SETTINGS_FILE)
+  __readJSON "$SETTINGS_JSON"
+}
+# Usage:
+# declare -A firstJSON  #this is an array
+# declare -A secondJSON #this is an array
+# Toolbelt_parseJSON "./test.json" firstJSON
+# Toolbelt_parseJSON "./test2.json" secondJSON
+# echo "--"
+# echo "${firstJSON[@]}"
+# echo "${secondJSON[@]}"
+
+__readJSON() {
+
+    local JSON=`jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" <<< "$1"`
+    local KEYS=''
+
+    if [ $# -gt 1 ]; then
+
+        KEYS="$2"
+    fi
+
+    while read -r PAIR; do
+        local KEY=''
+
+        if [ -z "$PAIR" ]; then
+            break
+        fi
+
+        IFS== read PAIR_KEY PAIR_VALUE <<< "$PAIR"
+
+        if [ -z "$KEYS" ]; then
+            KEY="$PAIR_KEY"
+        else
+            KEY="$KEYS:$PAIR_KEY"
+        fi
+
+
+                res=$(jq -e . 2>/dev/null <<< "$PAIR_VALUE")
+
+                exitCode=$?
+                check=`echo "$PAIR_VALUE" | grep -E ^\-?[0-9]*\.?[0-9]+$`
+          # if [ "${res}" ] && [ $exitCode -eq "0" ] && [[ ! "${PAIR_VALUE}" == ?(-)+([0-9]) ]]  ALTERNATIVE, works only for integer (not floating point)
+          if [ "${res}" ] && [ $exitCode -eq "0" ] && [[ "$check" == '' ]]
+            then
+                __readJSON "$PAIR_VALUE" "$KEY"
+               else
+            jsonArray["$KEY"]="$PAIR_VALUE"
+        fi
+
+
+
+    done <<< "$JSON"
+}
